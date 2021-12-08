82ebf971544f4f19d8d52c593f02f4f9059d3304
Javier Urquizu <javierurquizu@gmail.com>
2021-12-07 16:18:26 -0800

    Added StrobogrammaticNumberII solution

--- GoogleInterviewPrep/4_Recursion/StrobogrammaticNumberII/StrobogrammaticNumberII.py
+++ GoogleInterviewPrep/4_Recursion/StrobogrammaticNumberII/StrobogrammaticNumberII.py
@@ -17,10 +17,14 @@ Ex3:
   Input: n = 3
   Pairs: ["000", "010", "080",
           "101", "111", "181",
-          "808", "818", "888"]
+          "609", "619", "689",
+          "808", "818", "888",
+          "906", "916", "986"]
 
   Output: ["101", "111", "181",
-           "808", "818", "888"]
+           "609", "619", "689",
+           "808", "818", "888",
+           "906", "916", "986"]
 
 Ex4:
   Input: n = 4
@@ -29,7 +33,7 @@ Ex4:
           "6009", "6119", "6699", "6889", "6969",
           "8008", "8118", "8698", "8888", "8968",
           "9006", "9116", "9696", "9886", "9966"]
-          
+
   Output: ["1001", "1111", "1691", "1881", "1961",
            "6009", "6119", "6699", "6889", "6969",
            "8008", "8118", "8698", "8888", "8968",
@@ -47,5 +51,28 @@ class Solution:
         "8": "8",
         "9": "6"
     }
-    def findStrobogrammatic(self, n)
-        return list[str]
+
+    def findStrobogrammatic(self, n):
+        if n == 1:
+            return self.findStroboStrings(1)
+        else:
+            return [s for s in self.findStroboStrings(n) if (s[0] != "0")]
+
+    def findStroboStrings(self, n):
+        stroboList = []
+        if n == 1:
+            for s in self.stroboDigitsMap:
+                if s == self.stroboDigitsMap[s]:
+                    stroboList.append(s)
+        elif n == 2:
+            for s in self.stroboDigitsMap:
+                t = self.stroboDigitsMap[s]
+                stroboList.append(s+t)
+        else:
+            stroboList_n2 = self.findStroboStrings(n-2)
+            for s in self.stroboDigitsMap:
+                t = self.stroboDigitsMap[s]
+                for s2 in stroboList_n2:
+                    stroboList.append(s+s2+t)
+        return stroboList
+                
