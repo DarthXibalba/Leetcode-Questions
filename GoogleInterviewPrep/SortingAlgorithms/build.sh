be6ec5288a0eaabfa04b5d506890d74b3aea5734
Javier Urquizu <javierurquizu@gmail.com>
2022-01-17 11:35:38 -0800

    Added InsertionSort

--- GoogleInterviewPrep/SortingAlgorithms/build.sh
+++ GoogleInterviewPrep/SortingAlgorithms/build.sh
@@ -2,6 +2,7 @@
 if [ "$1" = "all" ]; then
     gcc SelectionSort.cpp -lstdc++ -o SelectionSort.o
     gcc BubbleSort.cpp -lstdc++ -o BubbleSort.o
+    gcc InsertionSort.cpp -lstdc++ -o InsertionSort.o
 elif [ "$1" = "clean" ]; then
     rm *.o
 else
