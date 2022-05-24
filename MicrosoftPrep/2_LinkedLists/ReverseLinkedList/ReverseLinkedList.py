7f5d1470e96ff7a52e8fcc0eea86ade413a41091
Javier Urquizu <jurquizu@outlook.com>
2022-05-24 13:50:06 -0700

    Added ReverseLinkedList skeleton code

---
+++ MicrosoftPrep/2_LinkedLists/ReverseLinkedList/ReverseLinkedList.py
@@ -0,0 +1,14 @@
+class ListNode:
+    def __init__(self, val=0, nxt=None)
+        self.value = val
+        self.next = nxt
+
+def reverseList(head):
+    pass
+
+def traverseList(head):
+    while(head.next != None)
+
+if __name__ == '__main__':
+    # EX1
+    head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
