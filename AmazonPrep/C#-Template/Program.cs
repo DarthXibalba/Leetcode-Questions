ab17fd51979241bcf34f783e942aa94a92b4cb0b
Javier Urquizu <jurquizu@outlook.com>
2022-06-15 23:13:03 -0700

    Added C#-Template

---
+++ AmazonPrep/C#-Template/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Project
+{
+    public class Solution
+    {
+        public var ProjectFunction()
+        {
+
+        }
+    }
+
+    static class Program
+    {
+        static void PrintOutput()
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            // Declare inputs
+            var myVar = new List<int[]>{};
+            // Compute
+            var sln = new Solution();
+            // Display output
+        }
+    }
+}
