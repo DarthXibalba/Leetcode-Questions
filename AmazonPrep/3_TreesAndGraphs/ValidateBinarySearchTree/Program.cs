be3dbd51f27e1ffac95fbabfc8d79d3c8e480e85
Javier Urquizu <jurquizu@outlook.com>
2022-06-17 13:47:10 -0700

    ValidateBinarySearchTree skeleton

---
+++ AmazonPrep/3_TreesAndGraphs/ValidateBinarySearchTree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ValidateBinarySearchTree
+{
+    public class Solution
+    {
+
+    }
+
+    public static class Program
+    {
+        public static void PrintOutput()
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+            var sln = new Solution();
+            // Declare input
+            var input_list = new List<>{};
+            // Compute
+            // Display Output
+            PrintOutput();
+        }
+    }
+}
