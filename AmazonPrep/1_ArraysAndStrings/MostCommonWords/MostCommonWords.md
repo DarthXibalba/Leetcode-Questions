f0aa2b9cae16f450d725038784fc39558d901e73
Javier Urquizu <jurquizu@outlook.com>
2022-06-17 13:32:33 -0700

    ReverseLinkedList

--- AmazonPrep/1_ArraysAndStrings/MostCommonWords/MostCommonWords.md
+++ AmazonPrep/1_ArraysAndStrings/MostCommonWords/MostCommonWords.md
@@ -3,17 +3,21 @@ Given a string `paragraph` and a string array of the banned words `banned`, retu
 The words in `paragraph` are **case-insensitive** and the answer should be returned in **lowercase**.  
 
 #### Example 1:
-**Input:** paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]  
-**Output:** "ball"  
-**Explanation:** "hit" occurs 3 times, but it is a banned word.
+```
+Input: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]  
+Output: "ball"  
+Explanation: "hit" occurs 3 times, but it is a banned word.
 "ball" occurs twice (and no other word does), so it is the most frequent non-banned word in the paragraph.  
 Note that words in the paragraph are not case sensitive,
 that punctuation is ignored (even if adjacent to words, such as "ball,"), 
 and that "hit" isn't the answer even though it occurs more because it is banned.  
+```
 
 #### Example 2:
-**Input:** paragraph = "a.", banned = []  
-**Output:** "a"  
+```
+Input: paragraph = "a.", banned = []  
+Output: "a"  
+```
 
 #### Constraints:
 - `1 <= paragraph.length <= 1000`
---
+++ AmazonPrep/2_LinkedLists/ReverseLinkedList/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace ReverseLinkedList
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode ReverseListIterative(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            ListNode revHead = new ListNode(head.val);
+            while (head.next != null)
+            {
+                head = head.next;
+                revHead = new ListNode(head.val, revHead);
+            }
+            return revHead;
+        }
+
+        public ListNode ReverseListRecursive(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+            else
+            {
+                ListNode prevNode = ReverseListRecursive(head.next);
+                head.next.next = head;
+                head.next = null;
+                return prevNode;
+            }
+        }
+
+        public ListNode ReverseList(ListNode head, int mode = 0)
+        {
+            if (mode == 0)
+            {
+                return ReverseListIterative(head);
+            }
+            else
+            {
+                return ReverseListRecursive(head);
+            }
+        }
+
+        public ListNode CreateLinkedList(int[] nums, int i = 0)
+        {
+            if (i < nums.Count() - 1)
+            {
+                return new ListNode(nums[i], CreateLinkedList(nums, i+1));
+            }
+            else
+            {
+                return new ListNode(nums[i]);
+            }
+        }
+    }
+
+    public static class Program
+    {
+        public static void PrintLinkedList(ListNode node)
+        {
+            if (node != null)
+            {
+                string output = $"{node.val}->";
+                while (node.next != null)
+                {
+                    node = node.next;
+                    output += $"{node.val}->";
+                }
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("[]");
+            }
+        }
+
+        public static void PrintOutput(List<ListNode> orig_head, List<ListNode> rev_head)
+        {
+            for (int i = 0; i < orig_head.Count(); i++)
+            {
+                PrintLinkedList(orig_head.ElementAt(i));
+                PrintLinkedList(rev_head.ElementAt(i));
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            // Declare inputs
+            var sln = new Solution();
+            var head_list = new List<ListNode>
+            {
+                sln.CreateLinkedList(new int[]{1,2,3,4,5}),
+                sln.CreateLinkedList(new int[]{1,2}),
+                null
+            };
+            var rev_list = new List<ListNode>();
+            // Compute
+            foreach (var head in head_list)
+            {
+                rev_list.Add(sln.ReverseList(head));
+            }
+            // Display output
+            PrintOutput(head_list, rev_list);
+        }
+    }
+}
