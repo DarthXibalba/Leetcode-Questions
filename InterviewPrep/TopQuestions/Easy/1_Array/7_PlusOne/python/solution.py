2fe99ff0db07d059094231ad14912dbdd5e8c546
Javier Urquizu <jurquizu@outlook.com>
2022-10-21 09:11:23 -0700

    Added assignment info for IntersectionOfTwoArraysII and PlusOne

---
+++ InterviewPrep/TopQuestions/Easy/1_Array/7_PlusOne/python/solution.py
@@ -0,0 +1,12 @@
+def plusOne(digits):
+    pass
+
+if __name__ == "__main__":
+    digits = [
+        [1,2,3],
+        [4,3,2,1],
+        [9]
+    ]
+    for d in digits:
+        print(d)
+        print(plusOne(d))
