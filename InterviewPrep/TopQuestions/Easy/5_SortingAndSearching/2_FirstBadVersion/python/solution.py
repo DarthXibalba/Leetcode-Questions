4a5846e1515030f35fd5d61f1da81ab2c57538e1
Javier Urquizu <jurquizu@outlook.com>
2022-10-22 14:00:58 -0700

    Added Easy/SortingAndSearching assignment info

---
+++ InterviewPrep/TopQuestions/Easy/5_SortingAndSearching/2_FirstBadVersion/python/solution.py
@@ -0,0 +1,11 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+def firstBadVersion(self, n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    pass
