1655d5cc6e08588452d5ea6c0c14b86880424d1f
Javier Urquizu <jurquizu@outlook.com>
2022-10-21 22:54:14 -0700

    Added assignment info for Easy/Trees category

---
+++ InterviewPrep/TopQuestions/Easy/4_Trees/3_SymmetricTree/python/solution.py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def isSymmetric(self, root):
+    """
+    :type root: TreeNode
+    :rtype: bool
+    """
+    pass
