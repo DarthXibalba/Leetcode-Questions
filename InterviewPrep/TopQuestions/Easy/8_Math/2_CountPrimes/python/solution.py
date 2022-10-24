45b30a3efca7ea3fa6980a1d4c7ad5fa4260bd89
Javier Urquizu <jurquizu@outlook.com>
2022-10-24 12:41:30 -0700

    Added solution to Easy/Math/CountPrimes

--- InterviewPrep/TopQuestions/Easy/8_Math/2_CountPrimes/python/solution.py
+++ InterviewPrep/TopQuestions/Easy/8_Math/2_CountPrimes/python/solution.py
@@ -1,6 +1,29 @@
-def countPrimes(self, n):
+# Damn this is a hard "Easy" problem
+def countPrimes(n):
     """
     :type n: int
     :rtype: int
     """
-    pass
+    if n < 3:
+        return 0
+    primes = [True] * n
+    primes[0] = primes[1] = False
+    for i in range(2, int(n ** 0.5) + 1):
+        if primes[i]:
+            primes[i * i: n: i] = [False] * len(primes[i * i: n: i])
+    return sum(primes)
+
+
+
+if __name__ == "__main__":
+    inputs = [
+        10,
+        0,
+        1,
+        2,
+        3
+    ]
+    for i in inputs:
+        print(i)
+        print(countPrimes(i))
+        print()
