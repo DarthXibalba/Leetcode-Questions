a09024351769219f67870d892e8d0489db9bc313
Javier Urquizu <jurquizu@outlook.com>
2022-10-24 16:46:26 -0700

    Added sln for Easy/Strings/LongestCommonPrefix

--- InterviewPrep/TopQuestions/Easy/2_Strings/8_LongestCommonPrefix/python/solution.py
+++ InterviewPrep/TopQuestions/Easy/2_Strings/8_LongestCommonPrefix/python/solution.py
@@ -1,5 +1,27 @@
 def longestCommonPrefix(strs):
-    pass
+    prefix = ""
+    if len(strs) == 0:
+        return prefix
+    numStrs = len(strs)
+    minStrLen = len(strs[0])
+    for s in strs:
+        if len(s) < minStrLen:
+            minStrLen = len(s)
+    for i in range(minStrLen):
+        c = strs[0][i]
+        isCP = True
+        for j in range(0, len(strs)):
+            if (c != strs[j][i]):
+                isCP = False
+                break
+        if isCP:
+            prefix += c
+        else:
+            break
+    return prefix
+        
+
+
 
 if __name__ == "__main__":
     strs = [
