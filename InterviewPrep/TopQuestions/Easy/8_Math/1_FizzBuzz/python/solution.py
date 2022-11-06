d9856cece0e0227aeaca741ba9a3be96b8b9f754
Javier Urquizu <jurquizu@outlook.com>
2022-11-05 21:02:57 -0700

    Added sln for Easy/Math/FizzBuzz

--- InterviewPrep/TopQuestions/Easy/8_Math/1_FizzBuzz/python/solution.py
+++ InterviewPrep/TopQuestions/Easy/8_Math/1_FizzBuzz/python/solution.py
@@ -1,6 +1,28 @@
-def fizzBuzz(self, n):
+def fizzBuzz(n):
     """
     :type n: int
     :rtype: List[str]
     """
-    pass
+    output = [None] * n
+    i = 1
+    while (i <= n):
+        if (i % 3 == 0):
+            if (i % 5 == 0):
+                output[i-1] = "FizzBuzz"
+            else:
+                output[i-1] = "Fizz"
+        elif (i % 5 == 0):
+            output[i-1] = "Buzz"
+        else:
+            output[i-1] = f"{i}"
+        i += 1
+    return output
+
+
+
+if __name__ == '__main__':
+    N = [3,5,15,1,120,99,100]
+    for n in N:
+        print(f"n = {n}")
+        print(f"{fizzBuzz(n)}")
+        print("")
