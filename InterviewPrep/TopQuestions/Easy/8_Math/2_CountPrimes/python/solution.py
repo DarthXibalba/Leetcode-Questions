72688aa224b051fb36fee44ba38773400a5b65ef
Javier Urquizu <jurquizu@outlook.com>
2022-11-06 22:12:03 -0800

    Added sln for Easy/Math/CountPrimes

--- InterviewPrep/TopQuestions/Easy/8_Math/2_CountPrimes/python/solution.py
+++ InterviewPrep/TopQuestions/Easy/8_Math/2_CountPrimes/python/solution.py
@@ -1,17 +1,30 @@
-# Damn this is a hard "Easy" problem
 def countPrimes(n):
     """
     :type n: int
     :rtype: int
     """
-    if n < 3:
+    if n <= 2:
         return 0
     primes = [True] * n
     primes[0] = primes[1] = False
-    for i in range(2, int(n ** 0.5) + 1):
+    i = 2
+    # Because of math, we only need to iterate from [2, n^1/2 + 1]
+    while (i < n**.5 // 1 + 1):
+        # If p[i] is marked as a prime, mark all multiples of it as False
         if primes[i]:
-            primes[i * i: n: i] = [False] * len(primes[i * i: n: i])
-    return sum(primes)
+            j = i*2
+            while (j < n):
+                primes[j] = False
+                j += i
+        i += 1
+    # Get the sum of number of primes
+    sum = 0
+    i = 0
+    while (i < n):
+        if primes[i]:
+            sum += 1
+        i += 1
+    return sum
 
 
 
