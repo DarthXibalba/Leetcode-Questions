dff8c55cdc9d45acbb20bdca04144a94017c7693
Javier Urquizu <jurquizu@outlook.com>
2022-11-08 17:11:26 -0800

    Added sln for Easy/Trees/MaximumDepthOfBinaryTree

--- InterviewPrep/TopQuestions/Easy/4_Trees/1_MaximumDepthOfBinaryTree/python/solution.py
+++ InterviewPrep/TopQuestions/Easy/4_Trees/1_MaximumDepthOfBinaryTree/python/solution.py
@@ -1,13 +1,43 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode():
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-def maxDepth(self, root):
+
+def maxDepth(root):
     """
     :type root: TreeNode
     :rtype: int
     """
-    pass
+    # Base cases
+    if root == None:
+        return 0
+    if (root.left == None) and (root.right == None):
+        return 1
+
+    leftDepth = 0
+    rightDepth = 0
+    if root.left != None:
+        leftDepth = maxDepth(root.left) + 1
+
+    if root.right != None:
+        rightDepth = maxDepth(root.right) + 1
+
+    # Determine which depth is deeper and return that
+    return max(leftDepth, rightDepth)
+
+
+
+if __name__ == '__main__':
+    tree1 = TreeNode(3)
+    tree1.left = TreeNode(9)
+    tree1.right = TreeNode(20, TreeNode(15), TreeNode(7))
+
+    tree2 = TreeNode(1, None, TreeNode(2))
+
+    print("Tree1")
+    print(maxDepth(tree1))
+    print("Tree2")
+    print(maxDepth(tree2))
