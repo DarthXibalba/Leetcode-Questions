eb55db0e8331ebf22178de372396feab4f3855c5
Javier Urquizu <jurquizu@outlook.com>
2022-11-14 14:04:45 -0800

    Added sln to Trees/[ValidateBST,SymmetricTree]

--- InterviewPrep/TopQuestions/Easy/4_Trees/2_ValidateBinarySearchTree/python/solution.py
+++ InterviewPrep/TopQuestions/Easy/4_Trees/2_ValidateBinarySearchTree/python/solution.py
@@ -1,13 +1,47 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode():
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-def isValidBST(self, root):
-    """
-    :type root: TreeNode
-    :rtype: bool
-    """
-    pass
+class Solution():
+    def __init__(self):
+        self.prev = None
+
+    # Use DFS InOrder Traversal to traverse the tree in ascending order
+    # This will allow us to check that each subsequent value is greater than the previous
+    def isValidBST(self, node):
+        if node == None:
+            return True
+        # Check that left subtree is valid, return False if it is not
+        if not self.isValidBST(node.left):
+            return False
+        # Now check this node to see that it is greater than prev
+        # Also assign self.prev if it has not been assigned yet and is set to None
+        if (self.prev == None) or (node.val > self.prev):
+            self.prev = node.val
+        else:
+            return False
+        # Check that the right subtree is valid
+        return self.isValidBST(node.right)
+
+
+if __name__ == '__main__':
+    tree1 = TreeNode(2, TreeNode(1), TreeNode(3))
+
+    tree2 = TreeNode(5)
+    tree2.left = TreeNode(1)
+    tree2.right = TreeNode(4, TreeNode(3), TreeNode(6))
+
+    tree3 = TreeNode(5)
+    tree3.left = TreeNode(4)
+    tree3.right = TreeNode(6, TreeNode(3), TreeNode(7))
+
+    trees = [tree1, tree2, tree3]
+
+    for i in range(len(trees)):
+        s = Solution()
+        print(f"Tree{i+1}")
+        print(s.isValidBST(trees[i]))
+        print("")
--- InterviewPrep/TopQuestions/Easy/4_Trees/3_SymmetricTree/python/solution.py
+++ InterviewPrep/TopQuestions/Easy/4_Trees/3_SymmetricTree/python/solution.py
@@ -1,13 +1,52 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from collections import deque
 
-def isSymmetric(self, root):
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def isSymmetricIter(root):
     """
     :type root: TreeNode
     :rtype: bool
     """
-    pass
+    q = deque()
+    q.append(root)
+    while q:
+        qLen = len(q)
+        level = deque()
+        for i in range(qLen):
+            node = q.popleft()
+            if node:
+                level.append(node.val)
+                q.append(node.left)
+                q.append(node.right)
+            else:
+                level.append(None)
+        # Process level stack for all levels past the 1st
+        while len(level) > 1:
+            if level.popleft() != level.pop():
+                return False
+    return True
+
+
+
+if __name__ == "__main__":
+    tree1 = TreeNode(1)
+    tree1.left = TreeNode(2, TreeNode(3), TreeNode(4))
+    tree1.right = TreeNode(2, TreeNode(4), TreeNode(3))
+
+    tree2 = TreeNode(1)
+    tree2.left = TreeNode(2, None, TreeNode(3))
+    tree2.right = TreeNode(2, None, TreeNode(3))
+
+    trees = [tree1, tree2]
+    answers = [True, False]
+
+    for i in range(len(trees)):
+        print(f"Tree{i}")
+        ans = isSymmetricIter(trees[i])
+        print(f"isSymmetric = {ans}")
+        assert answers[i] == ans, f"expected = {answers[i]} | result = {ans}"
