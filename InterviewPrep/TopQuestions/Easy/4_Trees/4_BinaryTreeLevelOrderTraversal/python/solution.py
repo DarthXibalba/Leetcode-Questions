52175cc58acdc4b4259b6e92c1ee37ca89d4a546
Javier Urquizu <jurquizu@outlook.com>
2022-11-14 16:45:35 -0800

    Added sln for Easy/Trees/BinaryTreeLevelOrderTraversal

--- InterviewPrep/TopQuestions/Easy/4_Trees/4_BinaryTreeLevelOrderTraversal/python/solution.py
+++ InterviewPrep/TopQuestions/Easy/4_Trees/4_BinaryTreeLevelOrderTraversal/python/solution.py
@@ -1,13 +1,44 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from collections import deque
 
-def levelOrder(self, root):
+class TreeNode():
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# Perform BFS
+def levelOrder(root):
     """
     :type root: TreeNode
     :rtype: List[List[int]]
     """
-    pass
+    levels = []
+    if root:
+        q = deque()
+        q.append(root)
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+            levels.append(level)
+    return levels
+
+
+
+if __name__ == "__main__":
+    trees = []
+    trees.append(TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7))))
+    trees.append(TreeNode(1))
+    trees.append(None)
+
+    for i in range(len(trees)):
+        print(f"Tree{i+1}")
+        print(levelOrder(trees[i]))
+        print("")
