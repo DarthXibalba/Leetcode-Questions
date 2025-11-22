0b2d5d2b3ca8d46fc02f6559e8464e7b42c7acd5
Javier Urquizu <javierurquizu@gmail.com>
2025-11-22 14:33:10 -0800

    +optimal solution_E1Q1.py

--- TopQuestionsByLevel/1_Easy/1_Array/01_RemoveDuplicatesFromSortedArray/python/solution_E1Q1.py
+++ TopQuestionsByLevel/1_Easy/1_Array/01_RemoveDuplicatesFromSortedArray/python/solution_E1Q1.py
@@ -1,31 +1,35 @@
 def RemoveDuplicates(sortedArray):
-    nonValue = "X"
-    i = 0  # Let i be the pointer to the non-duplicate portion of the array
-    for j in range(1, len(sortedArray)):
-        # If array[j] is the same as array[i], then overwrite with nonValue
-        if (sortedArray[i] == sortedArray[j]):
-            sortedArray[j] = nonValue
-        # Else, see if we need to perform any swaps
+    i = 0
+    j = i + 1
+    while(j < len(sortedArray)):
+        if sortedArray[i] == sortedArray[j]:
+            j += 1
         else:
-            # Increment non-duplicate pointer
             i += 1
-            # If i & j are no the same index, swap the values at these pointers
-            if (i != j):
-                sortedArray[i] = sortedArray[j]
-                sortedArray[j] = nonValue
-    return i + 1
+            sortedArray[i] = sortedArray[j]
+    return i+1
 
+class TestCase:
+    def __init__(self, input, k, nums):
+        self.input = input
+        self.k = k
+        self.nums = nums
+    
+    def print(self):
+        print("Input = " + str(self.input))
+        print("k = " + str(self.k))
+        print("Expected output = " + str(self.nums))
 
 if __name__ == "__main__":
-    input1 = [1,1,2]  # k = 2
-    input2 = [0,0,1,1,1,2,2,3,3,4]  # k = 5
-    input3 = [1,2]  # k = 2
-    ans1 = RemoveDuplicates(input1)
-    print(f"k1 = {ans1}")
-    print(f"output = {input1}")
-    ans2 = RemoveDuplicates(input2)
-    print(f"k2 = {ans2}")
-    print(f"output = {input2}")
-    ans3 = RemoveDuplicates(input3)
-    print(f"k2 = {ans3}")
-    print(f"output = {input3}")
+    tests = [TestCase([1,1,2], 2, [1,2]),
+             TestCase([0,0,1,1,1,2,2,3,3,4], 5, [0,1,2,3,4]),
+             TestCase([1,2], 2, [1,2])]
+    
+    for t in tests:
+        t.print()
+        k = RemoveDuplicates(t.input)
+        print("| ----- Output ----- |")
+        print("k = " + str(k))
+        print("Input = " + str(t.input))
+        print()
+    
